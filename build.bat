c07bdacb34f6d6c55ff04d807b0cbcd9c748c4d9
Riya1904 <140155870+Riya1904@users.noreply.github.com>
2025-04-29 22:12:38 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,14 @@
 @echo off
+echo =============================
 echo Compiling Java Program...
-javac Addition.java
-if %ERRORLEVEL% neq 0 (
-    echo Compilation failed.
+echo =============================
+javac -source 1.8 -target 1.8 Addition.java
+if %errorlevel% neq 0 (
+    echo Compilation Failed.
     exit /b 1
 )
+
+echo =============================
 echo Running Java Program...
+echo =============================
 java Addition
