824bbf9477bbe5f5e7b439ead5375778a853ccfc
Riya1904 <riy.ten.rt22@dypatil.edu>
2025-04-29 21:48:14 +0530

    Initial commit with Java addition program and build script

---
+++ Addition.java
@@ -0,0 +1,8 @@
+// Addition.java
+public class Addition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println("Sum: " + (a + b));
+    }
+}
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+javac Addition.java   # Compiles the Java program
+java Addition         # Executes the compiled class
